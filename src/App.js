5d8dd0d8d8af984e0f81d2bf046563e1221227f5
Raza2468 <faizeraza2468@gamil.com>
2021-07-09 18:37:23 +0500

    cardSetting

--- src/App.js
+++ src/App.js
@@ -48,37 +48,37 @@ function App() {
 
     <div>
       <nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand"><b>ID card Generate Website</b></a>
-    <form class="d-flex input-group w-auto">
-    <b> RollNo Name Course Batch </b>
-    </form>
-  </div>
-</nav>
+        <div class="container-fluid">
+          <a class="navbar-brand"><b>ID card Generate Website</b></a>
+          <label class="d-flex input-group w-auto">
+            <b> RollNo Name Course Batch </b>
+          </label>
+        </div>
+      </nav>
 
-<div>
-<form class="md-form">
-  <div class="file-field" >
-    <br />
-    <div class="d-flex justify-content-center">
-      <div class="btn btn-mdb-color btn-rounded float-left bg-success" > 
-        <span class="bg-success">Choose file</span>
-       
-        <input type="file"  onChange={(e) => {
-        const file = e.target.files[0];
-        readExcel(file)
-      }} />
-      
+      <div>
+        <form class="md-form">
+          <div class="file-field" >
+            <br />
+            <div class="d-flex justify-content-center">
+              <div class="btn btn-mdb-color btn-rounded float-left bg-success" >
+                <span class="bg-success">Choose file</span>
+
+                <input type="file" onChange={(e) => {
+                  const file = e.target.files[0];
+                  readExcel(file)
+                }} />
+
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
-  </div>
-</form>
-</div>
       <br />
       <br />
-      <br />
+      {/* <br /> */}
       <table className="table container">
-       
+
         <tbody>
           {items.map((d) => (
 
@@ -96,7 +96,7 @@ function App() {
                 <div class="w-75 h-50 float-left" >
                   {/* Width 45% */}
                   <ul class="list-group list-group-horizontal">
-                    <li class="list-group-item" id="name">Roll NO.:</li>
+                    <li class="list-group-item" id="name">Roll No:</li>
                     <li class="list-group-item" id="namedetail">{d.RollNo}</li>
                   </ul>
                   <ul class="list-group list-group-horizontal">
@@ -108,7 +108,7 @@ function App() {
                     <li class="list-group-item" id="namedetail">{d.Course}</li>
                   </ul>
                   <ul class="list-group list-group-horizontal">
-                    <li class="list-group-item" id="name">Batch::</li>
+                    <li class="list-group-item" id="name">Batch:</li>
                     <li class="list-group-item" id="namedetail">{d.Batch}</li>
                   </ul>
                 </div>
@@ -119,7 +119,9 @@ function App() {
               <div class="w-50 float-right" id="qr" >
 
                 <div class="w-50 p-3 float-left " ><img src="https://www.saylaniwelfare.com/public_html/images/saylani/Saylani-logo.png" height="48px" width="" /> </div>
-                <div class="w-50 p- float-right text-center" id="Smiit"><b>SMIT</b></div>
+                <div class="w-50 p-3 float-right text-center" id="smitprogram">SAYLANI MASS<br />IT TRAINING PROGRAM</div>
+                {/* <div class="w-50 p- float-right text-center" id="Smiit"><b>SMIT</b></div> */}
+
 
                 <div class="w-100 text-center" id="Qrcode"><b>Roll No.: <u> {d.RollNo}</u></b> </div>
                 {/* <QRCode id="abc" value={d.Qrcode} /> */}
@@ -132,10 +134,10 @@ function App() {
                 </div>
 
               </div>
-c------
+              c------
             </div>
 
-         
+
 
           ))}
         </tbody>
